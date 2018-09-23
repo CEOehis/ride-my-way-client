a43d6d488c90aa6722618e98d3ce6dfae4b6d5fc
Celestine Ekoh-Ordan <ceo.ehis@outlook.com>
2018-09-23 14:49:17 +0100

    feat(landing-page): toggle responsive navbar on mobile
    - add Github PR template

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,24 +1,46 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const Navbar = () => (
-  <nav className="layout-grid navbar">
-    <h1 className="brand"><Link to="/">RideMyWay</Link></h1>
-    <button className="menu btn" type="button">
-      <i className="fa fa-navicon" aria-hidden="true" />
-    </button>
-    <ul className="nav-links">
-      <li>
-        <Link to="/" className="brand">RideMyWay</Link>
-      </li>
-      <li>
-        <Link to="/register">Register</Link>
-      </li>
-      <li>
-        <Link to="/login">Login</Link>
-      </li>
-    </ul>
-  </nav>
-);
+class Navbar extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      mobileNavVisible: false,
+    };
+
+    this.toggleMenu = this.toggleMenu.bind(this);
+  }
+
+  toggleMenu() {
+    const { mobileNavVisible } = this.state;
+    this.setState({
+      mobileNavVisible: !mobileNavVisible,
+    });
+  }
+
+  render() {
+    const { mobileNavVisible } = this.state;
+    return (
+      <nav className="layout-grid navbar">
+        <h1 className="brand"><Link to="/">RideMyWay</Link></h1>
+        <button onClick={this.toggleMenu} className="menu btn" type="button">
+          <i className="fa fa-navicon" aria-hidden="true" />
+        </button>
+        <ul className={mobileNavVisible ? 'nav-links expanded' : 'nav-links'}>
+          <li>
+            <Link to="/" className="brand">RideMyWay</Link>
+          </li>
+          <li>
+            <Link to="/register">Register</Link>
+          </li>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+        </ul>
+      </nav>
+    );
+  }
+}
 
 export default Navbar;
